fc787e18c456c53b4b1fc19ae267fb2289f8124d
PabloHMartin <hermar65@gmail.com>
2021-09-05 19:37:19 +0200

    [UPD] movie-single component template and connected to service.

--- src/app/pages/movies/movies-routing.module.ts
+++ src/app/pages/movies/movies-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieSingleComponent } from './movie-single/movie-single.component';
 
 
 const routes: Routes = [
   { path: 'movies', component: MovieListComponent},
-  // { path: 'movies/:id', component: MovieSingleComponent},
+  { path: 'movies/:id', component: MovieSingleComponent},
   { path: '', redirectTo: '/movies',  pathMatch: 'full'},
 ];
 
